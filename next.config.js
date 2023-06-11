78d88139e2015c4573c7419c37d6dbe81b1ba111
“braden.lint“ <braden.lint@moogsoft.com>
2023-06-11 09:06:31 -0400

    trying tutorial: https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: { unoptimized: true }, 
-  output: 'export',}
-
-
-module.exports = nextConfig
+}
