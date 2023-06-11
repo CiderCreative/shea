c7b5f1ada804d9e67b08617c301586d6ab96fb7b
“braden.lint“ <braden.lint@moogsoft.com>
2023-06-11 08:48:49 -0400

    unoptimizing imgs and adding email api

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',}
+const nextConfig = {
+  images: { unoptimized: true }, 
+  output: 'export',}
 
 
 module.exports = nextConfig
